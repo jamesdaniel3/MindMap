bad30ad5f1b2c6f3fbbd592d1e3267cf555d9463
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-03-31 13:33:27 -0400

    fixed problems caused by a bad import

--- server/v2/config/db.ts
+++ server/v2/config/db.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
-import knex from "knex";
+import type { Knex } from "knex";
 
 dotenv.config();
 
-const dbConfig = {
+let dbConfig: Knex.Config = {
   client: "pg",
   connection: {
     host: process.env.DB_HOST,
@@ -18,19 +18,18 @@ const dbConfig = {
   },
   migrations: {
     tableName: "knex_migrations",
-    direction: "../db/migrations",
+    directory: "../db/migrations",
   },
   seeds: {
     directory: "../db/seeds",
   },
 };
 
-// production config
 if (
   process.env.NODE_ENV === "production" &&
   process.env.INSTANCE_CONNECTION_NAME
 ) {
-  const dbConfig = {
+  dbConfig = {
     client: "pg",
     connection: {
       user: process.env.DB_USER,
@@ -44,7 +43,7 @@ if (
     },
     migrations: {
       tableName: "knex_migrations",
-      direction: "../db/migrations",
+      directory: "../db/migrations",
     },
     seeds: {
       directory: "../db/seeds",
@@ -52,6 +51,4 @@ if (
   };
 }
 
-const db = knex(dbConfig);
-
-export default db;
+export default dbConfig; // Export the config, not the db instance
--- server/v2/services/healthCheck.ts
+++ server/v2/services/healthCheck.ts
@@ -1,4 +1,4 @@
-import db from "../config/db";
+import db from "../db/db";
 
 interface HeatlthStatus {
   status: "up" | "down";
