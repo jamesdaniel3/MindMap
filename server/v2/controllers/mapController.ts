7408d0aade63dec4bbed5bd64271e3cb9efd48d5
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-04-08 17:50:15 -0400

    created controller and service for map creation

--- server/v2/controllers/mapController.ts
+++ server/v2/controllers/mapController.ts
@@ -1,4 +1,5 @@
-import { getAllMaps } from "../services/mapServices";
+import { MapCreationRequest } from "../interfaces/mapInterfaces";
+import { getAllMaps, createMap } from "../services/mapServices";
 import { Context } from "koa";
 
 export const allMaps = async (ctx: Context): Promise<void> => {
@@ -7,13 +8,27 @@ export const allMaps = async (ctx: Context): Promise<void> => {
 
     if (allMapData.length === 0) {
       ctx.status = 201; // sentinel value to make it easy to check the case where there are no maps but there were no errors
-      ctx.body = "No maps found";
+      ctx.body = { allMapData: "No maps found" };
     } else {
       ctx.status = 200;
-      ctx.body = { mapData: allMapData };
+      ctx.body = { allMapData: allMapData };
     }
   } catch (err) {
-    console.error("Error retrieving maps:", err);
+    console.error("Error retrieving maps: ", err);
+    ctx.status = 500;
+    ctx.body = { error: "Internal Server Error" };
+  }
+};
+
+export const mapCreater = async (ctx: Context): Promise<void> => {
+  try {
+    const { creator_id, name } = ctx.request.body as MapCreationRequest;
+    const newMapData = await createMap({ creator_id: creator_id, name: name });
+
+    ctx.status = 200;
+    ctx.body = { newMapData: newMapData };
+  } catch (err) {
+    console.error("Error creating map: ", err);
     ctx.status = 500;
     ctx.body = { error: "Internal Server Error" };
   }
--- server/v2/services/mapServices.ts
+++ server/v2/services/mapServices.ts
@@ -1,8 +1,26 @@
 import db from "../db/db";
-import { DBMapList } from "../interfaces/mapInterfaces";
+import {
+  DBMapList,
+  MapCreationRequest,
+  DBMapModel,
+} from "../interfaces/mapInterfaces";
 
 export const getAllMaps = async (): Promise<DBMapList> => {
   const mapsData = await db("maps");
 
   return mapsData;
+};
+
+export const createMap = async ({
+  creator_id,
+  name,
+}: MapCreationRequest): Promise<DBMapModel> => {
+  const [newMap] = await db("maps")
+    .insert({
+      creator_id: creator_id,
+      name: name,
+    })
+    .returning("*");
+
+  return newMap;
 };
