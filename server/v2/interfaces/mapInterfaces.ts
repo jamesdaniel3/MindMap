899db88b765393435f3d5cd9ab56d9724a548b55
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-04-08 17:50:04 -0400

    added interfaces needed for creating maps

--- server/v2/interfaces/mapInterfaces.ts
+++ server/v2/interfaces/mapInterfaces.ts
@@ -1,5 +1,12 @@
-interface DBMapModel {
+export interface DBMapModel {
   id: number;
+  creator_id: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface MapCreationRequest {
   creator_id: string;
   name: string;
 }
