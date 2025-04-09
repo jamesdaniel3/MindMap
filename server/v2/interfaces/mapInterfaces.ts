ee667f77caa2b6933fcfa51f5eac81d58cbf496e
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-04-09 15:11:57 -0400

    added necesary interfaces

--- server/v2/interfaces/mapInterfaces.ts
+++ server/v2/interfaces/mapInterfaces.ts
@@ -1,9 +1,14 @@
+import { DBNodeModel } from "./nodeInterfaces";
 export interface DBMapModel {
   id: number;
   creator_id: string;
   name: string;
   created_at: string;
   updated_at: string;
+}
+
+export interface AllMapInfo extends DBMapModel {
+  nodes: DBNodeModel[];
 }
 
 export interface MapCreationRequest {
@@ -13,6 +18,11 @@ export interface MapCreationRequest {
 
 export interface MapRetrievalRequest {
   mapId: number;
+}
+
+export interface AllMapInfoRetrievalRequest {
+  mapId: number;
+  userId: string;
 }
 
 export type DBMapList = DBMapModel[];
