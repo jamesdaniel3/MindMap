343c50107d020c0da9f7ae356a89b8219d4c8814
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-04-08 20:21:19 -0400

    added routes for creating nodes

--- server/v2/routes/index.ts
+++ server/v2/routes/index.ts
@@ -3,6 +3,7 @@ import healthRoutes from "./healthRoutes";
 import adminRoutes from "./adminRoutes";
 import userRoutes from "./userRoutes";
 import mapRoutes from "./mapRoutes";
+import nodeRoutes from "./nodeRoutes";
 
 const router = new Router({
   prefix: "/api/v2",
@@ -13,5 +14,6 @@ router.use(healthRoutes.routes(), healthRoutes.allowedMethods());
 router.use(adminRoutes.routes(), adminRoutes.allowedMethods());
 router.use(userRoutes.routes(), userRoutes.allowedMethods());
 router.use(mapRoutes.routes(), mapRoutes.allowedMethods());
+router.use(nodeRoutes.routes(), nodeRoutes.allowedMethods());
 
 export default router;
