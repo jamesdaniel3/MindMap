62effefabf59211d91f5ee0a2051f30713aeb37d
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-04-10 21:53:35 -0400

    updated the assignment display for the sake of a production build

--- frontend/src/routes/AssignmentDisplay.jsx
+++ frontend/src/routes/AssignmentDisplay.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 
-import fake_data from "../assets/fake_json2.json";
+// import fake_data from "../assets/fake_json2.json";
 import { useEffect, useState } from "react";
 import StaticContentScreen from "../components/StaticAssignmentScreen";
 
@@ -10,15 +10,21 @@ export default function AssignmentDisplay() {
   const { assignment_id } = useParams();
   const [assignmentData, setAssignmentData] = useState({});
 
-  useEffect(() => {
-    setAssignmentData(fake_data);
-  }, []);
-
   return (
     <>
-      {assignmentData.type === 0 && (
-        <StaticContentScreen assignmentInfo={assignmentData} />
-      )}
+      <p>This page is under construction</p>
     </>
   );
+
+  // useEffect(() => {
+  //   setAssignmentData(fake_data);
+  // }, []);
+
+  // return (
+  //   <>
+  //     {assignmentData.type === 0 && (
+  //       <StaticContentScreen assignmentInfo={assignmentData} />
+  //     )}
+  //   </>
+  // );
 }
